82c5b20d1135ebf94d1e3180c9ded11a6faa9c13
itsmartagonzalez <alu0101254487@ull.edu.es>
2021-03-30 12:35:04 +0000

    Fix

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Jane",
         "excerpt":"Jane has worked on Jekyll for the past five years.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-github-pages-y-netlify-itsmartagonzalez/authors/jane.html",
+        "url": "/authors/jane.html",
         "teaser": null
       },{
         "title": "Jill",
         "excerpt":"Jill is the author of some blog entries.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-github-pages-y-netlify-itsmartagonzalez/authors/jill.html",
+        "url": "/authors/jill.html",
         "teaser": null
       },{
         "title": "Marta",
         "excerpt":"Marta is the author of this page.  ","categories": [],
         "tags": [],
-        "url": "/jekyll-github-pages-y-netlify-itsmartagonzalez/authors/marta.html",
+        "url": "/authors/marta.html",
         "teaser": null
       },{
         "title": "Banana",
         "excerpt":"A banana is an edible fruit – botanically a berry – produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called “plantains”, distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but...","categories": ["food","posts"],
         "tags": ["fruits"],
-        "url": "/jekyll-github-pages-y-netlify-itsmartagonzalez/food/posts/2021/03/12/banana.html",
+        "url": "/food/posts/2021/03/12/banana.html",
         "teaser": null
       },{
         "title": "Pizza",
         "excerpt":"Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (such as anchovies, mushrooms, onions, olives, pineapple, meat, etc.), which is then baked at a high temperature, traditionally in a wood-fired oven....","categories": ["food","posts"],
         "tags": ["junk food"],
-        "url": "/jekyll-github-pages-y-netlify-itsmartagonzalez/food/posts/2021/03/12/pizza.html",
+        "url": "/food/posts/2021/03/12/pizza.html",
         "teaser": null
       }]
